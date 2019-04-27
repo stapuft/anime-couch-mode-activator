4811c6c46ae721267e1f1f93014077a8f7ff4fac
stapuft <46431157+stapuft@users.noreply.github.com>
2019-04-27 02:54:32 -0400

    Add files via upload
    had to break script into 2 parts to squash bugs.

---
+++ anime couch mode activator-pard deux.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         anime couch mode activator-pard deux-fullscreen
+// @namespace    http://tampermonkey.net/
+// @version      1.6
+// @description  Adds extra keyboard shortcuts to html5 videos and automatically enters fullscreen
+// @author       Daehock
+// @Thanks       Skillrax  & v4lt5u
+// @include      *://www.rapidvideo.*
+// @include      *://mcloud.to/
+// @include      *://openload.*
+// @include      *://streamango.*
+// @include      *://prettyfast.to/*
+// @include      *://www.mp4upload.com/*
+// @include      *://9anime.ru/watch/*
+// @include      *://9anime.nl/watch/*
+// @include      *://9anime.to/watch/*
+// @include      *://9anime.live/watch/*
+// @include      *://kissanime.ru/Anime/*
+// @include      *://kimcartoon.to/Cartoon/*
+// @include      *://streamp1ay.me/*
+// @include      http://*/*
+// @include      https://*/*
+// @grant        none
+// ==/UserScript==
+
+
+var elem = document.getElementsByTagName("video")[0];
+if (elem.requestFullscreen) {
+  elem.requestFullscreen();
+} else if (elem.msRequestFullscreen) {
+  elem.msRequestFullscreen();
+} else if (elem.mozRequestFullScreen) {
+  elem.mozRequestFullScreen();
+} else if (elem.webkitRequestFullscreen) {
+  elem.webkitRequestFullscreen();
+}
+
+var myVideo = document.getElementsByTagName("video")[0];
+document.getElementsByTagName("video")[0].setAttribute("controls","controls");
+document.getElementsByTagName("video")[0].hover(function(){	myVideo.controls = !myVideo.controls; },5000);
+
