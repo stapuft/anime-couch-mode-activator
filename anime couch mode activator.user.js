ebbd0276836dae2e3c755864cd37313b49a489df
stapuft <46431157+stapuft@users.noreply.github.com>
2019-04-27 19:07:30 -0400

    1.7 keyboard

--- anime couch mode activator.user.js
+++ anime couch mode activator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         anime couch mode activator-pard un-keyboard
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @description  Adds extra keyboard shortcuts to html5 videos and automatically enters fullscreen
 // @author       Daehock
 // @Thanks       Skillrax  & v4lt5u
@@ -18,8 +18,6 @@
 // @include      *://kissanime.ru/Anime/*
 // @include      *://kimcartoon.to/Cartoon/*
 // @include      *://streamp1ay.me/*
-// @include      http://*/*
-// @include      https://*/*
 // @grant        none
 // ==/UserScript==
 
@@ -43,21 +41,7 @@ function skip2(element) {
     element.currentTime += 30;
 }
 
-function mute(element) {
-    element.volume = 0;
-}
 
-function unmute(element) {
-    element.volume = 1.0;
-}
-
-function play(element) {
-    element.play();
-}
-
-function pause(element) {
-    element.pause();
-}
 
 window.addEventListener('keypress', (event)=>{
     if (event.key == "h") {
