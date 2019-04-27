e693e9c3a83da0565fd1966e0accf8f3f090c0d4
stapuft <46431157+stapuft@users.noreply.github.com>
2019-04-27 01:22:26 -0400

    Add files via upload
    version 1.6, fixed O.OP. to make the control bar actually appear normally.

--- anime couch mode activator.user.js
+++ anime couch mode activator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         anime couch mode activator
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  Adds extra keyboard shortcuts to html5 videos and automatically enters fullscreen
 // @author       Daehock
 // @Thanks       Skillrax  & v4lt5u
@@ -18,12 +18,10 @@
 // @include      *://kissanime.ru/Anime/*
 // @include      *://kimcartoon.to/Cartoon/*
 // @include      *://streamp1ay.me/*
-// @include      http://*/*
-// @include      https://*/*
 // @grant        none
 // ==/UserScript==
 
-    document.getElementsByTagName("video")[0].focus();
+document.getElementsByTagName("video")[0].focus();
 
 var elem = document.getElementsByTagName("video")[0];
 if (elem.requestFullscreen) {
@@ -35,6 +33,10 @@ if (elem.requestFullscreen) {
 } else if (elem.webkitRequestFullscreen) {
   elem.webkitRequestFullscreen();
 }
+
+var myVideo = document.getElementsByTagName("video")[0];
+document.getElementsByTagName("video")[0].setAttribute("controls","controls");
+document.getElementsByTagName("video")[0].hover(function(){	myVideo.controls = !myVideo.controls; },5000);
 
 
 function rewind(element) {
@@ -53,10 +55,26 @@ function skip2(element) {
     element.currentTime += 30;
 }
 
+function mute(element) {
+    element.volume = 0;
+}
+
+function unmute(element) {
+    element.volume = 1.0;
+}
+
+function play(element) {
+    element.play();
+}
+
+function pause(element) {
+    element.pause();
+}
+
 window.addEventListener('keypress', (event)=>{
     if (event.key == "h") {
         rewind(document.getElementsByTagName("video")[0]);
-    } else if (event.key == "l") {
+    }else if (event.key == "l") {
         skip(document.getElementsByTagName("video")[0]);
     }else if (event.key == "k") {
         skip2(document.getElementsByTagName("video")[0]);
@@ -65,9 +83,7 @@ window.addEventListener('keypress', (event)=>{
     }
 
 
-var myVideo = document.getElementsByTagName("video")[0];
-document.getElementsByTagName("video")[0].setAttribute("controls","controls");
-document.getElementsByTagName("video")[0].hover(function(){	myVideo.controls = !myVideo.controls; },5000);
+document.getElementsByTagName("video")[0].focus("video")[0];
 
 
 });
